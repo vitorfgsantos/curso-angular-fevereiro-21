450e26681bca9d53d56259bdd33cd3d530ecd1a7
Vitor Farias <vitorfgsantos@outlook.com>
2021-02-12 20:42:12 -0300

    primeiros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'minha-aplicacao';
+
+  title = 'Seja bem-vindo!';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Mudou o valor...');
+      this.title = 'Outro título';
+    }, 3000);
+  }
+
+  somar(numero1: number, numero2: number) {    
+    return numero1 + numero2;
+  }
+
 }
