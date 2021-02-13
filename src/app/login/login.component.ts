543e1e7b8cf7f9bac83d1673ba4d1b0670e94291
Vitor Farias <vitorfgsantos@outlook.com>
2021-02-13 18:00:31 -0300

    validacao de formularios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('usuarioInput') usuarioInput: ElementRef | undefined;
+
+  usuario: string = '';
+  senha: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      form.controls.usuario.markAsTouched();
+      form.controls.senha.markAsTouched();
+
+      if (form.controls.usuario.invalid) {
+        this.usuarioInput?.nativeElement.focus();
+      }
+
+      return;
+    }
+
+    this.login();
+  }
+
+  exibeErro(nomeControle: string, form: NgForm) {
+    if (!form.controls[nomeControle]) {
+      return false;
+    }
+
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
+  }
+
+  login() {
+    // this.usuario
+    // this.senha
+    console.log('Login')
   }
 
 }
