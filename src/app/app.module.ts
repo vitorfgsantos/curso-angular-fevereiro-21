4bcdd07e9d8ce1f87fde3b0501bcdd2144c4e4d0
Vitor Farias <vitorfgsantos@outlook.com>
2021-02-13 15:07:38 -0300

    reorganizando estrutura de pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
-import { ExercicioDataBindingComponent } from './exercicio-data-binding/exercicio-data-binding.component';
-import { ExercicioContadorComponent } from './exercicio-contador/exercicio-contador.component';
-import { FormsModule } from '@angular/forms';
-import { ExercicioDiretivasComponent } from './exercicio-diretivas/exercicio-diretivas.component';
-import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
-import { registerLocaleData } from '@angular/common';
-
-import localePT from '@angular/common/locales/pt';
+import { LancamentosComponent } from './lancamentos/lancamentos.component';
+import { LoginComponent } from './login/login.component';
+import { PlanosContaComponent } from './planos-conta/planos-conta.component';
+import { ExercicioContadorComponent } from './shared/components/exercicio-contador/exercicio-contador.component';
+import { ExercicioDataBindingComponent } from './shared/components/exercicio-data-binding/exercicio-data-binding.component';
+import { ExercicioDiretivasComponent } from './shared/components/exercicio-diretivas/exercicio-diretivas.component';
+import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 registerLocaleData(localePT, 'pt');
 
@@ -27,6 +31,11 @@ registerLocaleData(localePT, 'pt');
     ExercicioContadorComponent,
     ExercicioDiretivasComponent,
     ExercicioPipesComponent,
+    LoginComponent,
+    CadastroComponent,
+    LancamentosComponent,
+    PlanosContaComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
